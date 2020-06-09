ac203b67e68bd187318bd0cf4aa40f08203408a2
waledak <tanguy.cirillo@gmail.com>
2020-06-09 21:15:36 +0200

    first route done

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req,res)=>{
+  const display = { message : "Hello World!" }
+  res.status(200).json(display)
+})
+
 module.exports = app;
